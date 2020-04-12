c1e65c8465a65b2bb6736dd6e8b3ca5f19c0dd47
AlexJ2005 <alexanderjernstrom@gmail.com>
2020-04-12 20:50:30 +0200

    added a little more text on the homepage

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -5,6 +5,11 @@ const HomePage = () => {
   return (
     <div>
       <Typography variant="h3">Home page</Typography>
+      <Typography variant="body1">
+        Welcome to music collab, here you'll find songwriters, singers and
+        producers you can collab with. To get started make an account and then
+        go to collab.
+      </Typography>
     </div>
   );
 };
